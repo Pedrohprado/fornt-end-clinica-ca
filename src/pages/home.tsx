039619319308e25d561eec77687a852e21a63a04
Pedro Henrique Prado <pedroh.prado@outlook.com>
2024-06-08 15:50:44 -0300

    i try undestand how TS working in react

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,26 @@
-const Home = () => {
+import { useState } from 'react';
+
+interface Props {
+  name: string;
+  age: number;
+}
+const Home = ({ name, age }: Props) => {
+  const [isValue, setValue] = useState<string | null>(null);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
+  //   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  //     event.preventDefault();
+  //   };
   return (
     <div className=' w-full h-screen bg-black text-gray-400'>
-      <h1>Bem vindo</h1>
+      <input type='text' onChange={handleChange} />
+      <p>{isValue}</p>
+      <p>
+        {name} {age}
+      </p>
     </div>
   );
 };
